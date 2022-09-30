d5fe5ae70f706d0517f9dd210bf859e44b300532
Rutuja Narawade <114046019+Rutuja-1511@users.noreply.github.com>
2022-09-30 11:00:11 +0530

    Add files via upload

---
+++ 29-09-2022/LabAssgn5_29.java
@@ -0,0 +1,30 @@
+package Daily_Assignment;
+
+import java.util.Scanner;
+
+public class LabAssgn5_29 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int size,a,sum = 0,avg;
+		
+		System.out.println("Enter size : ");
+		size=sc.nextInt();
+		
+		int no[]=new int[size];
+		System.out.println("Enter "+size+" numbers:");
+		
+		for(a=0;a<size;a++) {
+			no[a]=sc.nextInt();
+		}
+		for(a = 0; a < no.length; a++) {
+			sum += no[a];
+		}
+		avg=sum/size;
+		
+		System.out.println("Sum of all elements in array = "+sum);
+		System.out.println("Average of all elements in array = "+avg);
+	}
+
+}
