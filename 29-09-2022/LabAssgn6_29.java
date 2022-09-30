2d67cd5c6bb6ba4063e5189555903995d96021e8
Rutuja Narawade <114046019+Rutuja-1511@users.noreply.github.com>
2022-09-30 11:04:36 +0530

    Add files via upload

---
+++ 29-09-2022/LabAssgn6_29.java
@@ -0,0 +1,26 @@
+package Daily_Assignment;
+
+public class LabAssgn6_29 {
+
+
+	public static void main(String[] args) 
+{
+		// TODO Auto-generated method stub
+		int i,ans,n=150;
+        System.out.println("Prime numbers between 1 to 150 are ");
+        for(int j=2;j<=n;j++)
+        {
+        	ans=0;
+        	for(i=1;i<=j;i++)
+        	{
+        		if(j%i==0)
+        		{
+        			ans++;        
+        		}
+        	}
+        	if(ans==2)
+        		System.out.print(j+"  ");     
+        }
+	}
+
+}
